199d3fc03df19a8c88bac404e98a388a67173ce6
Mohamed Lem <68510473+MedtheVorg@users.noreply.github.com>
2024-02-29 13:04:38 +0100

    added  routing pages

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,3 +1,16 @@
-export default function ArchivePage() {
-    return <div>ArchivePage</div>;
+import Logo from '@/components/Logo';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Archive | Notes Management Platform',
+};
+
+export default async function ArchivePage() {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    return (
+        <div className=' h-screen flex items-center justify-center '>
+            ArchivePage
+        </div>
+    );
 }
---
+++ src/app/create/page.tsx
@@ -0,0 +1,9 @@
+import CreateForm from '@/components/CreateForm';
+
+export default function createPage() {
+    return (
+        <div className=' flex items-center justify-center pt-10'>
+            <CreateForm />
+        </div>
+    );
+}
